d26c7c1718ab0ff77db488f37a68702e22d90789
root <root@vps596432.ovh.net>
2020-07-03 10:29:56 +0200

    controller for comments

--- controllers/CommentController.php
+++ controllers/CommentController.php
@@ -1,40 +1,85 @@
 <?php
 
+namespace Controllers;
+
+use Models;
+
+
+class CommentController extends ConfigController
+{
+
+
 function addComment($content, $blog)
 {
+
+    $message = message();
+
     $auteur = $_SESSION['id'];
-    $commentManager = new CommentManager();
+    $commentManager = new Models\CommentManager();
     $affectedLines = $commentManager->storeComment($content, $blog, $auteur);
 
-    return $affectedLines;
+    if (isset($_SESSION['pseudo'])) {
+
+        header('Location: annonce/'.$_POST['blog']);
+    }
+    else{
+
+        echo $this->twig->render('backend/connexionUserView.twig', ['message' => $message['message_comment_error']]);
+    }
+
+
 }
 
 function adminComment()
 {
-    $commentManager = new CommentManager();
+
+
+    $message =  message();
+
+    $commentManager = new Models\CommentManager();
 
     $comment = $commentManager->adminComment();
+    if ($_SESSION['admin'] == 1) {
 
-    require('views/backend/adminView.php');
+        echo $this->twig->render('backend/adminView.twig', ['comment' => $comment]);
+
+
+   }
+   else{
+       echo $this->twig->render('frontend/home.twig', ['message' => $message['message_admin_refus']]);
+
+   }
+
+
 }
 
 function updateComment($id)
 {
 
-    $commentManager = new CommentManager();
+    
+    $commentManager = new Models\CommentManager();
 
     $comment = $commentManager->updateComment($id);
 
-    header('Location: index.php?action=admin');
+    $comments = $this->adminComment();
+
+    return $comments;
+
+
 }
 
 function deleteComment($id)
 {
+   
 
-    $commentManager = new CommentManager();
+    $commentManager = new Models\CommentManager();
 
     $comment = $commentManager->deleteComment($id);
 
-    header('Location: index.php?action=admin');
+    $comments = $this->adminComment();
+
+    return $comments;
+
 }
 
+}
